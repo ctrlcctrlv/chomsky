9f06bbc318a64c2264a6724ff931bb37bbb4c25a
Fredrick Brennan <copypaste@kittens.ph>
2021-03-15 07:13:19 -0400

    Version 2.2
    * Redrew all glyphs with regular cubic contours, leading to smaller
      filesize
    * Added "of", "and" and "the" discretionary ligatures

--- build.sh
+++ build.sh
@@ -8,8 +8,12 @@ fontforge -lang=py -script build.py
 if [[ "$STOP" =~ ^[Yy]$ ]]; then
     exit
 fi
-# Generate a WOFF, if posssible
+# Generate WOFF/WOFF2, if posssible
 hash sfnt2woff 2>/dev/null && sfnt2woff dist/Chomsky.otf
+hash woff2_compress 2>/dev/null && woff2_compress dist/Chomsky.otf
+# Compress if possible
+hash compreffor 2>/dev/null && compreffor dist/Chomsky.otf
+mv dist/Chomsky.compressed.otf dist/Chomsky.otf
 
 INSTALLDIR=$HOME/.fonts/c
 if [[ "$INSTALL" =~ ^[Yy]$ ]]; then
